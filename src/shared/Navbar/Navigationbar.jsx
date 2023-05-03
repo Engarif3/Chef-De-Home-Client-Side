d79dc61a34d1ecdbb838facf3d4d6eccc2a59934
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-05-03 22:07:36 +0200

    active link added

--- src/shared/Navbar/Navigationbar.jsx
+++ src/shared/Navbar/Navigationbar.jsx
@@ -1,17 +1,18 @@
 import React, { useContext } from "react";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
 import { FaUserCircle } from "react-icons/fa";
 import { AuthContext } from "../../Provider/AuthProvider";
 
-
 const Navigationbar = () => {
   const { user, logOut } = useContext(AuthContext);
 
   const handleLogOut = () => {
-    logOut().then().catch(error => console.log(error));
+    logOut()
+      .then()
+      .catch((error) => console.log(error));
   };
 
   const renderTooltip = (props) => (
@@ -21,26 +22,38 @@ const Navigationbar = () => {
     </Tooltip>
   );
 
-
   return (
-    <div
-    
-    >
+    <div>
       <div>
-        <Navbar collapseOnSelect expand="lg" bg="black" variant="light" style={{position: "relative", top: 0}}>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="black"
+          variant="light"
+          style={{ position: "relative", top: 0 }}
+        >
           <Container>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" className="bg-white" />
+            <Navbar.Toggle
+              aria-controls="responsive-navbar-nav"
+              className="bg-white"
+            />
             <Navbar.Collapse id="responsive-navbar-nav">
-            <h1 className="text-danger fst-italic">Chef De Home</h1>
-              <Nav className="mx-auto d-flex align-items-center gap-4 ">
-                
-                <Link className="text-decoration-none text-white" to={"/"}>
+              <h1 className="text-danger fst-italic">Chef De Home</h1>
+              <Nav className="mx-auto d-flex align-items-center gap-4">
+                <NavLink
+                  className="nav-link text-decoration-none text-white"
+                  to={"/"}
+                >
                   Home
-                </Link>
-
-                <Link className="text-decoration-none text-white" to={"/blog"} >Blog</Link>
-                {/* <Link className="text-decoration-none text-white" to={"/private"}>Private</Link> */}
+                </NavLink>
+                <NavLink
+                  className="nav-link text-decoration-none text-white"
+                  to={"/blog"}
+                >
+                  Blog
+                </NavLink>
               </Nav>
+
               <Nav className="d-flex  align-items-center gap-2">
                 {user && (
                   <OverlayTrigger
@@ -48,7 +61,10 @@ const Navigationbar = () => {
                     delay={{ show: 50, hide: 200 }}
                     overlay={renderTooltip}
                   >
-                    <Button className="d-flex justify-content-center align-items-center rounded-circle m-0 p-0  " variant="primary" >
+                    <Button
+                      className="d-flex justify-content-center align-items-center rounded-circle m-0 p-0  "
+                      variant="primary"
+                    >
                       <FaUserCircle
                         style={{ fontSize: "2.5rem" }}
                         className="m-0 p-0"
@@ -58,7 +74,9 @@ const Navigationbar = () => {
                 )}
 
                 {user ? (
-                  <Button variant="danger" onClick={handleLogOut}>Logout</Button>
+                  <Button variant="danger" onClick={handleLogOut}>
+                    Logout
+                  </Button>
                 ) : (
                   <Link to={"/login"}>
                     <Button variant="danger">Login</Button>
