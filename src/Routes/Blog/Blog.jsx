d4ff35fafdc4729288061d931ee088ad080a1918
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-05-03 11:04:13 +0200

    blog pages added but no answer of the questions

--- src/Routes/Blog/Blog.jsx
+++ src/Routes/Blog/Blog.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Container } from "react-bootstrap";
 
 const Blog = () => {
-    return (
-        <div>
-            Blog is coming .....
-        </div>
-    );
+  return (
+    <Container className="text -center">
+      <p>1. Tell us the differences between uncontrolled and controlled components.</p>
+      <p>2. How to validate React props using PropTypes </p>
+      <p>3. Tell us the difference between nodejs and express js.</p>
+      <p>4. What is a custom hook, and why will you create a custom hook?</p>
+    </Container>
+  );
 };
 
 export default Blog;
